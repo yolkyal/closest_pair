47127b2fb08bf0cd583456184843444fc77ff451
yolkyal <yolkyal@hotmail.co.uk>
2022-08-22 15:35:29 +0100

    Create ClosestPairSolverFactory.java

---
+++ src/solver/ClosestPairSolverFactory.java
@@ -0,0 +1,26 @@
+package solver;
+
+public final class ClosestPairSolverFactory {
+
+    private ClosestPairSolverFactory() {}
+
+    public static ClosestPairSolver createClosestPairSolver(SolverStrategy strategy) {
+        switch (strategy) {
+            case NAIVE:
+                return createNaiveClosestPairSolver();
+            case RANDOMISED_LINEAR:
+                return createRandomisedLinearClosestPairSolver();
+            default:
+                throw new RuntimeException("SolverStrategy does not match any registered ClosestPairSolver: " + strategy.name());
+        }
+    }
+
+    private static ClosestPairSolver createNaiveClosestPairSolver() {
+        return new ClosestPairSolverNaive();
+    }
+
+    private static ClosestPairSolver createRandomisedLinearClosestPairSolver() {
+        return new ClosestPairSolverRandomisedLinear();
+    }
+
+}
